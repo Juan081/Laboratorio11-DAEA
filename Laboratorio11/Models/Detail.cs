85af4d43bd1c99b9f084226eafbbf758e0de6a1c
juanx <juanx@DIEGO-DESKTOP>
2023-11-07 21:55:27 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio11/Models/Detail.cs
@@ -0,0 +1,19 @@
+﻿namespace Laboratorio11.Models
+{
+    public class Detail
+    {
+        public int DetailID { get; set; }
+
+        public int ProductID { get; set; }
+        public Product Product { get; set; }    
+
+        public int InvoiceID { get; set; }  
+        public Invoice Invoice { get; set; }
+
+        public int Amount { get; set; }
+
+        public float Price { get; set; }
+
+        public float SubTotal { get; set; } 
+    }
+}
---
+++ Laboratorio11/Models/Invoice.cs
@@ -0,0 +1,18 @@
+﻿namespace Laboratorio11.Models
+{
+    public class Invoice
+    {
+        public int InvoiceID { get; set; }
+
+        public DateTime InvoiceName { get; set;}
+
+        public string InvoiceNumber { get; set;}
+
+        
+        public float Total {  get; set;}
+
+        public int CustomerID { get; set;}
+        public Customer Customer { get; set;}
+
+    }
+}
